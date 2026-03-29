54481841f4dfc24ad34e7650822a49916f7013e9
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-03-29 19:35:28 +0300

    Update ModCommands.java

--- src/main/java/com/frametrip/dragonlegacyquesttoast/command/ModCommands.java
+++ src/main/java/com/frametrip/dragonlegacyquesttoast/command/ModCommands.java
@@ -1,9 +1,11 @@
 package com.frametrip.dragonlegacyquesttoast.command;
 
 import com.frametrip.dragonlegacyquesttoast.network.ModNetwork;
+import com.frametrip.dragonlegacyquesttoast.network.QuestToastConfigPacket;
 import com.frametrip.dragonlegacyquesttoast.network.QuestToastPacket;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
@@ -17,28 +19,77 @@ public class ModCommands {
                         .requires(source -> source.hasPermission(2))
                         .then(Commands.argument("player", EntityArgument.player())
                                 .then(Commands.argument("type", StringArgumentType.word())
-                                        .then(Commands.argument("title", StringArgumentType.greedyString())
-                                                .executes(ctx -> {
-                                                    ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
-                                                    String type = StringArgumentType.getString(ctx, "type");
-                                                    String title = StringArgumentType.getString(ctx, "title");
+                                        .executes(ctx -> {
+                                            ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
+                                            String type = StringArgumentType.getString(ctx, "type");
 
-                                                    if (!"accepted".equals(type) && !"completed".equals(type)) {
-                                                        ctx.getSource().sendFailure(net.minecraft.network.chat.Component.literal("Type must be accepted or completed"));
-                                                        return 0;
-                                                    }
+                                            if (!"accepted".equals(type) && !"completed".equals(type)) {
+                                                ctx.getSource().sendFailure(net.minecraft.network.chat.Component.literal("Type must be accepted or completed"));
+                                                return 0;
+                                            }
 
-                                                    ModNetwork.CHANNEL.send(
-                                                            PacketDistributor.PLAYER.with(() -> player),
-                                                            new QuestToastPacket(type, title)
-                                                    );
+                                            ModNetwork.CHANNEL.send(
+                                                    PacketDistributor.PLAYER.with(() -> player),
+                                                    new QuestToastPacket(type, "")
+                                            );
 
-                                                    ctx.getSource().sendSuccess(() ->
-                                                            net.minecraft.network.chat.Component.literal("Quest toast sent to " + player.getGameProfile().getName()),
-                                                            true
-                                                    );
-                                                    return 1;
-                                                }))))
+                                            return 1;
+                                        })))
+        );
+
+        dispatcher.register(
+                Commands.literal("dlquesttoastconfig")
+                        .requires(source -> source.hasPermission(2))
+                        .then(Commands.argument("player", EntityArgument.player())
+                                .then(Commands.literal("reset")
+                                        .executes(ctx -> {
+                                            ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
+
+                                            ModNetwork.CHANNEL.send(
+                                                    PacketDistributor.PLAYER.with(() -> player),
+                                                    new QuestToastConfigPacket(true, 0, 0, 0, 0, 0, 0, 0, 0)
+                                            );
+
+                                            return 1;
+                                        }))
+                                .then(Commands.literal("set")
+                                        .then(Commands.argument("x", IntegerArgumentType.integer())
+                                                .then(Commands.argument("y", IntegerArgumentType.integer())
+                                                        .then(Commands.argument("width", IntegerArgumentType.integer(1))
+                                                                .then(Commands.argument("height", IntegerArgumentType.integer(1))
+                                                                        .then(Commands.argument("fadeIn", IntegerArgumentType.integer(1))
+                                                                                .then(Commands.argument("stay", IntegerArgumentType.integer(1))
+                                                                                        .then(Commands.argument("fadeOut", IntegerArgumentType.integer(1))
+                                                                                                .then(Commands.argument("startOffsetX", IntegerArgumentType.integer())
+                                                                                                        .executes(ctx -> {
+                                                                                                            ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
+
+                                                                                                            int x = IntegerArgumentType.getInteger(ctx, "x");
+                                                                                                            int y = IntegerArgumentType.getInteger(ctx, "y");
+                                                                                                            int width = IntegerArgumentType.getInteger(ctx, "width");
+                                                                                                            int height = IntegerArgumentType.getInteger(ctx, "height");
+                                                                                                            int fadeIn = IntegerArgumentType.getInteger(ctx, "fadeIn");
+                                                                                                            int stay = IntegerArgumentType.getInteger(ctx, "stay");
+                                                                                                            int fadeOut = IntegerArgumentType.getInteger(ctx, "fadeOut");
+                                                                                                            int startOffsetX = IntegerArgumentType.getInteger(ctx, "startOffsetX");
+
+                                                                                                            ModNetwork.CHANNEL.send(
+                                                                                                                    PacketDistributor.PLAYER.with(() -> player),
+                                                                                                                    new QuestToastConfigPacket(
+                                                                                                                            false,
+                                                                                                                            x,
+                                                                                                                            y,
+                                                                                                                            width,
+                                                                                                                            height,
+                                                                                                                            fadeIn,
+                                                                                                                            stay,
+                                                                                                                            fadeOut,
+                                                                                                                            startOffsetX
+                                                                                                                    )
+                                                                                                            );
+
+                                                                                                            return 1;
+                                                                                                        })))))))))))
         );
     }
 }
