ea0868a36a12fec6e637e85ebcd1cb35121ae6ad
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-04-06 01:46:57 +0300

    Register new packets in ModNetwork
    Added new packet registration for OpenQuestToastEditorPacket and OpenNpcDialogueEditorPacket.

--- src/main/java/com/frametrip/dragonlegacyquesttoast/network/ModNetwork.java
+++ src/main/java/com/frametrip/dragonlegacyquesttoast/network/ModNetwork.java
@@ -18,76 +18,16 @@ public class ModNetwork {
     private static int packetId = 0;
 
     public static void init() {
-        CHANNEL.registerMessage(
-                packetId++,
-                QuestToastPacket.class,
-                QuestToastPacket::encode,
-                QuestToastPacket::decode,
-                QuestToastPacket::handle
-        );
-
-        CHANNEL.registerMessage(
-                packetId++,
-                QuestToastConfigPacket.class,
-                QuestToastConfigPacket::encode,
-                QuestToastConfigPacket::decode,
-                QuestToastConfigPacket::handle
-        );
-
-        CHANNEL.registerMessage(
-                packetId++,
-                NpcDialoguePacket.class,
-                NpcDialoguePacket::encode,
-                NpcDialoguePacket::decode,
-                NpcDialoguePacket::handle
-        );
-
-        CHANNEL.registerMessage(
-                packetId++,
-                NpcDialogueConfigPacket.class,
-                NpcDialogueConfigPacket::encode,
-                NpcDialogueConfigPacket::decode,
-                NpcDialogueConfigPacket::handle
-        );
-
-        CHANNEL.registerMessage(
-                packetId++,
-                OpenAwakeningScreenPacket.class,
-                OpenAwakeningScreenPacket::encode,
-                OpenAwakeningScreenPacket::decode,
-                OpenAwakeningScreenPacket::handle
-        );
-
-        CHANNEL.registerMessage(
-                packetId++,
-                AwakeningBackgroundConfigPacket.class,
-                AwakeningBackgroundConfigPacket::encode,
-                AwakeningBackgroundConfigPacket::decode,
-                AwakeningBackgroundConfigPacket::handle
-        );
-
-        CHANNEL.registerMessage(
-                packetId++,
-                AwakeningCenterConfigPacket.class,
-                AwakeningCenterConfigPacket::encode,
-                AwakeningCenterConfigPacket::decode,
-                AwakeningCenterConfigPacket::handle
-        );
-
-        CHANNEL.registerMessage(
-                packetId++,
-                AwakeningPathsConfigPacket.class,
-                AwakeningPathsConfigPacket::encode,
-                AwakeningPathsConfigPacket::decode,
-                AwakeningPathsConfigPacket::handle
-        );
-
-        CHANNEL.registerMessage(
-                packetId++,
-                OpenUiEditorMenuPacket.class,
-                OpenUiEditorMenuPacket::encode,
-                OpenUiEditorMenuPacket::decode,
-                OpenUiEditorMenuPacket::handle
-        );
+        CHANNEL.registerMessage(packetId++, QuestToastPacket.class, QuestToastPacket::encode, QuestToastPacket::decode, QuestToastPacket::handle);
+        CHANNEL.registerMessage(packetId++, QuestToastConfigPacket.class, QuestToastConfigPacket::encode, QuestToastConfigPacket::decode, QuestToastConfigPacket::handle);
+        CHANNEL.registerMessage(packetId++, NpcDialoguePacket.class, NpcDialoguePacket::encode, NpcDialoguePacket::decode, NpcDialoguePacket::handle);
+        CHANNEL.registerMessage(packetId++, NpcDialogueConfigPacket.class, NpcDialogueConfigPacket::encode, NpcDialogueConfigPacket::decode, NpcDialogueConfigPacket::handle);
+        CHANNEL.registerMessage(packetId++, OpenAwakeningScreenPacket.class, OpenAwakeningScreenPacket::encode, OpenAwakeningScreenPacket::decode, OpenAwakeningScreenPacket::handle);
+        CHANNEL.registerMessage(packetId++, AwakeningBackgroundConfigPacket.class, AwakeningBackgroundConfigPacket::encode, AwakeningBackgroundConfigPacket::decode, AwakeningBackgroundConfigPacket::handle);
+        CHANNEL.registerMessage(packetId++, AwakeningCenterConfigPacket.class, AwakeningCenterConfigPacket::encode, AwakeningCenterConfigPacket::decode, AwakeningCenterConfigPacket::handle);
+        CHANNEL.registerMessage(packetId++, AwakeningPathsConfigPacket.class, AwakeningPathsConfigPacket::encode, AwakeningPathsConfigPacket::decode, AwakeningPathsConfigPacket::handle);
+        CHANNEL.registerMessage(packetId++, OpenUiEditorMenuPacket.class, OpenUiEditorMenuPacket::encode, OpenUiEditorMenuPacket::decode, OpenUiEditorMenuPacket::handle);
+        CHANNEL.registerMessage(packetId++, OpenQuestToastEditorPacket.class, OpenQuestToastEditorPacket::encode, OpenQuestToastEditorPacket::decode, OpenQuestToastEditorPacket::handle);
+        CHANNEL.registerMessage(packetId++, OpenNpcDialogueEditorPacket.class, OpenNpcDialogueEditorPacket::encode, OpenNpcDialogueEditorPacket::decode, OpenNpcDialogueEditorPacket::handle);
     }
 }
