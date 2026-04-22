10c7f4e2062d6fd377cb7eb51d6851f9974229bb
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-04-22 08:33:59 +0300

    Add points parameter to SyncAbilitiesPacket

--- src/main/java/com/frametrip/dragonlegacyquesttoast/network/SyncAbilitiesPacket.java
+++ src/main/java/com/frametrip/dragonlegacyquesttoast/network/SyncAbilitiesPacket.java
@@ -10,30 +10,30 @@ import java.util.function.Supplier;
  
 public class SyncAbilitiesPacket {
     private final Set<String> abilities;
+    private final int points;
  
-    public SyncAbilitiesPacket(Set<String> abilities) {
+    public SyncAbilitiesPacket(Set<String> abilities, int points) {
         this.abilities = new HashSet<>(abilities);
+        this.points = points;
     }
  
     public static void encode(SyncAbilitiesPacket msg, FriendlyByteBuf buf) {
         buf.writeInt(msg.abilities.size());
-        for (String id : msg.abilities) {
-            buf.writeUtf(id);
-        }
+        for (String id : msg.abilities) buf.writeUtf(id);
+        buf.writeInt(msg.points);
     }
  
     public static SyncAbilitiesPacket decode(FriendlyByteBuf buf) {
         int count = buf.readInt();
         Set<String> abilities = new HashSet<>();
-        for (int i = 0; i < count; i++) {
-            abilities.add(buf.readUtf());
-        }
-        return new SyncAbilitiesPacket(abilities);
+        for (int i = 0; i < count; i++) abilities.add(buf.readUtf());
+        int points = buf.readInt();
+        return new SyncAbilitiesPacket(abilities, points);
     }
  
     public static void handle(SyncAbilitiesPacket msg, Supplier<NetworkEvent.Context> ctxSupplier) {
         NetworkEvent.Context ctx = ctxSupplier.get();
-        ctx.enqueueWork(() -> ClientPlayerAbilityState.setAbilities(msg.abilities));
+        ctx.enqueueWork(() -> ClientPlayerAbilityState.sync(msg.abilities, msg.points));
         ctx.setPacketHandled(true);
     }
 }
