fd4e6ef6e2ffef1853b67e6c8cdacb37911a1b38
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-04-22 08:29:22 +0300

    Update DragonLegacyQuestToastMod.java

--- src/main/java/com/frametrip/dragonlegacyquesttoast/DragonLegacyQuestToastMod.java
+++ src/main/java/com/frametrip/dragonlegacyquesttoast/DragonLegacyQuestToastMod.java
@@ -5,18 +5,22 @@ import com.frametrip.dragonlegacyquesttoast.client.QuestToastOverlay;
 import com.frametrip.dragonlegacyquesttoast.command.ModCommands;
 import com.frametrip.dragonlegacyquesttoast.network.ModNetwork;
 import com.frametrip.dragonlegacyquesttoast.network.SyncAbilitiesPacket;
-import com.frametrip.dragonlegacyquesttoast.server.FireStrikeHandler;
+import com.frametrip.dragonlegacyquesttoast.server.AbilityRegistry;
+import com.frametrip.dragonlegacyquesttoast.server.FireAbilityHandler;
+import com.frametrip.dragonlegacyquesttoast.server.IceAbilityHandler;
 import com.frametrip.dragonlegacyquesttoast.server.PlayerAbilityManager;
+import com.frametrip.dragonlegacyquesttoast.server.StormAbilityHandler;
+import com.frametrip.dragonlegacyquesttoast.server.VoidAbilityHandler;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.network.PacketDistributor;
  
 @Mod(DragonLegacyQuestToastMod.MODID)
@@ -27,9 +31,14 @@ public class DragonLegacyQuestToastMod {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
  
         ModNetwork.init();
+ 
         MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
         MinecraftForge.EVENT_BUS.addListener(this::onPlayerLogin);
-        MinecraftForge.EVENT_BUS.register(new FireStrikeHandler());
+ 
+        MinecraftForge.EVENT_BUS.register(new FireAbilityHandler());
+        MinecraftForge.EVENT_BUS.register(new IceAbilityHandler());
+        MinecraftForge.EVENT_BUS.register(new StormAbilityHandler());
+        MinecraftForge.EVENT_BUS.register(new VoidAbilityHandler());
  
         if (FMLEnvironment.dist == Dist.CLIENT) {
             modBus.addListener(this::registerOverlays);
@@ -37,7 +46,7 @@ public class DragonLegacyQuestToastMod {
     }
  
     private void registerOverlays(RegisterGuiOverlaysEvent event) {
-        event.registerAboveAll("quest_toast_overlay", QuestToastOverlay.OVERLAY);
+        event.registerAboveAll("quest_toast_overlay",  QuestToastOverlay.OVERLAY);
         event.registerAboveAll("npc_dialogue_overlay", NpcDialogueOverlay.OVERLAY);
     }
  
@@ -46,11 +55,17 @@ public class DragonLegacyQuestToastMod {
     }
  
     private void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        if (event.getEntity() instanceof ServerPlayer serverPlayer) {
-            ModNetwork.CHANNEL.send(
-                    PacketDistributor.PLAYER.with(() -> serverPlayer),
-                    new SyncAbilitiesPacket(PlayerAbilityManager.getAbilities(serverPlayer.getUUID()))
-            );
-        }
+        if (!(event.getEntity() instanceof ServerPlayer player)) return;
+ 
+        // Creative players receive all abilities; survival players receive their own set
+        java.util.Set<String> abilities = player.isCreative()
+                ? AbilityRegistry.getAllIds()
+                : PlayerAbilityManager.getAbilities(player.getUUID());
+        int points = PlayerAbilityManager.getPoints(player.getUUID());
+ 
+        ModNetwork.CHANNEL.send(
+                PacketDistributor.PLAYER.with(() -> player),
+                new SyncAbilitiesPacket(abilities, points)
+        );
     }
 }
