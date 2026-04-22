15df9029761fb928274d4a99fa12dfcdc3b889ca
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-04-22 15:14:52 +0300

    Update ClientPlayerAbilityState.java

--- src/main/java/com/frametrip/dragonlegacyquesttoast/client/ClientPlayerAbilityState.java
+++ src/main/java/com/frametrip/dragonlegacyquesttoast/client/ClientPlayerAbilityState.java
@@ -1,34 +1,37 @@
 package com.frametrip.dragonlegacyquesttoast.client;
- 
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
- 
+
 public class ClientPlayerAbilityState {
     private static Set<String> unlockedAbilities = new HashSet<>();
+    private static Set<String> disabledAbilities = new HashSet<>();
     private static int awakeningPoints = 0;
- 
-    public static void sync(Set<String> abilities, int points) {
+
+    public static void sync(Set<String> abilities, Set<String> disabled, int points) {
         unlockedAbilities = new HashSet<>(abilities);
+        disabledAbilities = new HashSet<>(disabled);
         awakeningPoints = points;
     }
- 
+
     public static boolean hasAbility(String abilityId) {
         return unlockedAbilities.contains(abilityId);
     }
- 
+
+    public static boolean isAbilityDisabled(String abilityId) {
+        return disabledAbilities.contains(abilityId);
+    }
+
     public static Set<String> getAbilities() {
         return Collections.unmodifiableSet(unlockedAbilities);
     }
- 
+
+    public static Set<String> getDisabledAbilities() {
+        return Collections.unmodifiableSet(disabledAbilities);
+    }
+
     public static int getPoints() {
         return awakeningPoints;
     }
 }
-    }
- 
-    public static int getPoints() {
-        return awakeningPoints;
-    }
-}
- 
