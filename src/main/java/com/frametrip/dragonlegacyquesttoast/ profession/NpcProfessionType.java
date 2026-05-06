5f6cc69dc5c565c310bc73a1ab14d005df0b3c00
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-05-06 08:07:54 +0300

    Update NpcProfessionType.java

--- src/main/java/com/frametrip/dragonlegacyquesttoast/ profession/NpcProfessionType.java
+++ src/main/java/com/frametrip/dragonlegacyquesttoast/ profession/NpcProfessionType.java
@@ -2,12 +2,14 @@ package com.frametrip.dragonlegacyquesttoast.profession;
 
 public enum NpcProfessionType {
     NONE,
-    TRADER;
+    TRADER,
+    BUILDER;
 
     public String label() {
         return switch (this) {
-            case NONE   -> "Нет профессии";
-            case TRADER -> "Купля-продажа";
+            case NONE    -> "Нет профессии";
+            case TRADER  -> "Купля-продажа";
+            case BUILDER -> "Строитель";
         };
     }
 }
