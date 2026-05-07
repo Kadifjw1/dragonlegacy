1a3ef345fb15485d7389fc741be63478e7d9afde
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-05-07 13:43:10 +0300

    Update AnimationState.java

--- src/main/java/com/frametrip/dragonlegacyquesttoast/server/animation/AnimationState.java
+++ src/main/java/com/frametrip/dragonlegacyquesttoast/server/animation/AnimationState.java
@@ -1,16 +1,20 @@
 package com.frametrip.dragonlegacyquesttoast.server.animation;
 
 public enum AnimationState {
-    IDLE, WALK, WORK, TALK, GUARD, SLEEP, CUSTOM;
+    IDLE, WALK, RUN, SIT, SLEEP, TALK, WORK, FOLLOW, ATTACK, GUARD, CUSTOM;
 
     public String label() {
         return switch (this) {
             case IDLE   -> "Стоит";
             case WALK   -> "Идёт";
-            case WORK   -> "Работает";
-            case TALK   -> "Говорит";
-            case GUARD  -> "Охраняет";
+            case RUN    -> "Бежит";
+            case SIT    -> "Сидит";
             case SLEEP  -> "Спит";
+            case TALK   -> "Говорит";
+            case WORK   -> "Работает";
+            case FOLLOW -> "Следует";
+            case ATTACK -> "Атакует";
+            case GUARD  -> "Охраняет";
             case CUSTOM -> "Произвольная";
         };
     }
