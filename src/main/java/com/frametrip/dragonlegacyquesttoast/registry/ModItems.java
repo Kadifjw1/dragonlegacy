b5877c0ba4e74a2827b00f2f3474240e8c1c564d
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-05-07 13:41:20 +0300

    Update ModItems.java

--- src/main/java/com/frametrip/dragonlegacyquesttoast/registry/ModItems.java
+++ src/main/java/com/frametrip/dragonlegacyquesttoast/registry/ModItems.java
@@ -1,6 +1,8 @@
 package com.frametrip.dragonlegacyquesttoast.registry;
  
 import com.frametrip.dragonlegacyquesttoast.DragonLegacyQuestToastMod;
+import com.frametrip.dragonlegacyquesttoast.item.EventBookItem;
+import com.frametrip.dragonlegacyquesttoast.item.GuiConfiguratorItem;
 import com.frametrip.dragonlegacyquesttoast.item.LegacyCoinItem;
 import com.frametrip.dragonlegacyquesttoast.item.NpcSpawnerItem;
 import com.frametrip.dragonlegacyquesttoast.item.TravelerJournalItem;
@@ -27,5 +29,15 @@ public class ModItems {
     public static final RegistryObject<Item> TRAVELER_JOURNAL =
         ITEMS.register("traveler_journal", () ->
             new TravelerJournalItem(new Item.Properties().stacksTo(1))
+        );
+
+ public static final RegistryObject<Item> EVENT_BOOK =
+        ITEMS.register("event_book", () ->
+            new EventBookItem(new Item.Properties().stacksTo(1))
+        );
+
+    public static final RegistryObject<Item> GUI_CONFIGURATOR =
+        ITEMS.register("gui_configurator", () ->
+            new GuiConfiguratorItem(new Item.Properties().stacksTo(1))
         );
 }
